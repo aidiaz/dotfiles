fd705d937ea5c8b4869e2848c2788a0d13799734
aidiaz <54038697+aidiaz@users.noreply.github.com>
2025-01-04 16:33:01 -0300

    zsh, git, ohmyposh, tmux dotfiles

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+# ln -s ~/dotfiles/.vimrc ~/.vimrc
+ls -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.config/tmux/.tmux.conf
+ln -s ~/dotfiles/ohmyposh/catppuccin.omp.json ~/.config/ohmyposh/catppuccin.omp.json
