5e06866fe523b32e0854c72950c9217c7e2d86fc
aidiaz <54038697+aidiaz@users.noreply.github.com>
2025-01-05 00:44:35 -0300

    tested dotfiles and iterm color for osx

--- .zshrc
+++ .zshrc
@@ -24,8 +24,38 @@ autoload -Uz compinit && compinit
 
 zinit cdreplay -q
 
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
 # Keybindings
-#bindkey -e
-#bindkey '^p' history-search-backward
-#bindkey '^n' history-search-forward
-#bindkey '^[w' kill-region
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+bindkey '^[w' kill-region
+# Aliases
+alias ls='ls --color'
+alias vim='nvim'
+alias c='clear'
+
+# Shell integrations
+eval "$(fzf --zsh)"
+eval "$(zoxide init --cmd cd zsh)"
+
+export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,40 @@
 #!/bin/zsh
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-# ln -s ~/dotfiles/.vimrc ~/.vimrc
-ls -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tmux/.tmux.conf ~/.config/tmux/.tmux.conf
-ln -s ~/dotfiles/ohmyposh/catppuccin.omp.json ~/.config/ohmyposh/catppuccin.omp.json
+# List of files to check
+
+config_folder="$HOME/.config"
+
+# Check if the folder exists
+if [ ! -d "$config_folder" ]; then
+  # If it doesn't exist, create the folder
+  mkdir -p "$config_folder"
+  mkdir -p "$config_folder/tmux"
+  mkdir -p "$config_folder/ohmyposh"
+  echo "Folder created: $config_folder"
+else
+  echo "Folder already exists: $config_folder, removing and recreating"
+  rm -rf "$config_folder"
+  mkdir -p "$config_folder"
+  mkdir -p "$config_folder/tmux"
+  mkdir -p "$config_folder/ohmyposh"
+fi
+
+
+files=("$HOME/.zshrc" "$HOME/.gitconfig" "$HOME/.config/tmux/tmux.conf" "$HOME/.config/ohmyposh/catppuccin.omp.json")
+
+# Loop through each file
+for file in "${files[@]}"; do
+  # Check if the file exists
+  if [ ! -f "$file" ]; then
+    echo "$file doesn't exist, creating alias"
+  else
+    echo "File already exists: $file, removing for alias creation"
+    rm "$file"
+  fi
+done
+
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+# ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
+ln -s $HOME/dotfiles/ohmyposh/catppuccin.omp.json $HOME/.config/ohmyposh/catppuccin.omp.json
