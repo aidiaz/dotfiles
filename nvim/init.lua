0c1ed2a18651631544e5c21389076907bf07409f
aidiaz <adiaz@strongbyform.com>
2025-02-13 14:31:01 -0300

    neovim black comply with flake8, flake8 config file

--- nvim/init.lua
+++ nvim/init.lua
@@ -788,10 +788,18 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "black", stop_after_first = true },
+				python = { "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			formatters = {
+				black = {
+					prepend_args = {
+						"--line-length",
+						"79",
+					},
+				},
 			},
 		},
 	},
--- nvim/lua/kickstart/plugins/lint.lua
+++ nvim/lua/kickstart/plugins/lint.lua
@@ -5,6 +5,13 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			local lint = require("lint")
+			lint.linters.flake8.args = function()
+				local config_path = vim.fn.findfile(".flake8", ".;")
+				if config_path == "" then
+					return {}
+				end
+				return { "--config", config_path }
+			end
 			lint.linters_by_ft = {
 				markdown = { "markdownlint" },
 				python = { "flake8" },
