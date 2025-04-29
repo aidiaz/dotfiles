5a38dbfcff732d193a17bbb77860c48384649727
aidiaz <54038697+aidiaz@users.noreply.github.com>
2025-04-29 10:16:31 -0400

    remove lsp_signature

--- nvim/init.lua
+++ nvim/init.lua
@@ -651,18 +651,6 @@ require("lazy").setup({
 		},
 	},
 	{
-		"ray-x/lsp_signature.nvim",
-		event = "InsertEnter",
-		opts = {
-			bind = true,
-			handler_opts = {
-				border = "rounded",
-			},
-		},
-		-- or use config
-		-- config = function(_, opts) require'lsp_signature'.setup({you options}) end
-	},
-	{
 		-- Main LSP Configuration
 		"neovim/nvim-lspconfig",
 		dependencies = {
